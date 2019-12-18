4f3327e72135a62ee3cbd6c1ac7a69fa7a154c1b
szidonia.laszlo <szidonia.laszlo@directtwo.hu>
2019-12-18 10:32:34 +0100

    Initial commit

---
+++ app/src/main/java/com/example/szidonialaszlo/phonevibrateproject/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.szidonialaszlo.phonevibrateproject;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    android.widget.EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.okBtn);
+        editText = (android.widget.EditText)findViewById(R.id.timeEditText);
+    }
+
+    public void startAlert(View view){
+
+        int i = Integer.parseInt(editText.getText().toString());
+        Intent intent = new Intent(this, MyBroadcastReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                this.getApplicationContext(), 234324243, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()
+                + (i * 1000), pendingIntent);
+        Toast.makeText(this, "Alarm set in " + i + " seconds",
+                Toast.LENGTH_LONG).show();
+
+    }
+}
